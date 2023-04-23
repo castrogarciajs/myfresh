0d0ae3cd2c8f6f4f3a7c59d5b29bdd909dc1b280
Sebastian009w <ia3266595@gmail.com>
2023-04-22 21:33:17 -0500

    styles header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,15 +1,11 @@
 import Lemon from "https://deno.land/x/tabler_icons_tsx@0.0.1/tsx/lemon-2.tsx";
 
 function Header() {
-  const menus = [
-    { name: "Home", href: "/" },
-    { name: "Components", href: "/components" },
-    { name: "Docs", href: "/docs" },
-  ];
+  const menus = [{ name: "Home", href: "/" }];
   return (
     <header class="header">
       <div class="header-title">
-        <Lemon />
+        <Lemon size={35} />
         <div class="myfresh">My Fresh</div>
       </div>
       <ul class="menu">
